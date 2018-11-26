b4fbeaa51532a4f99efbbd9cfa0209156237e6ee
ievdokdm <ievdokdm@gmail.com>
2018-11-25 16:37:14 -0800

    simple mvc page

---
+++ DotNet/Other/SimpleMVC/SimpleMVC/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleMVC.Models;
+
+namespace SimpleMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PinModel model)
+        {
+            string message;
+
+            if (ModelState.IsValid)
+            {
+                message = "Pin :" + model.Pin;
+            }
+            else
+            {
+                message = "Failed to create the product. Please try again";
+            }
+            return Content(message);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductEditModel model)
+        {
+            string message;
+
+            if (ModelState.IsValid)
+            {
+                message = "product " + model.Name + " Rate " + model.Rate.ToString() + " With Rating " + model.Rating.ToString() + " created successfully";
+            }
+            else
+            {
+                message = "Failed to create the product. Please try again";
+            }
+            return Content(message);
+        }
+    }
+}
---
+++ DotNet/Other/SimpleMVC/SimpleMVC/Models/PinModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleMVC.Models
+{
+    public class PinModel
+    {
+        public string Pin { get; set; }
+    }
+}
